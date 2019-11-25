6f4e19f02b39bd217d15634676760463cdc5ac31
Sergio Brighenti <sergio@brighenti.me>
2019-11-25 20:54:30 +0100

    Fixed error in mysql migration

--- resources/schemas/mysql/mysql.4.sql
+++ resources/schemas/mysql/mysql.4.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `users`
-    ADD COLUMN `remember_selector` VARCHAR(16),
-    ADD COLUMN `remember_token` VARCHAR(256),
-    ADD COLUMN `remember_expire` TIMESTAMP;
+    ADD COLUMN `remember_selector` VARCHAR(16) DEFAULT NULL,
+    ADD COLUMN `remember_token` VARCHAR(256) DEFAULT NULL,
+    ADD COLUMN `remember_expire` TIMESTAMP NULL DEFAULT NULL;
 
 ALTER TABLE `users` ADD INDEX (`remember_selector`);
 
