afefbfa99dfd7652821103d4a53a6718db6f4ffd
Sergio Brighenti <sergio@brighenti.me>
2020-04-06 19:24:26 +0200

    Fixed issue #177

--- app/Middleware/RememberMiddleware.php
+++ app/Middleware/RememberMiddleware.php
@@ -20,19 +20,19 @@ class RememberMiddleware extends Middleware
         if (!$this->session->get('logged', false) && !empty($request->getCookieParams()['remember'])) {
             [$selector, $token] = explode(':', $request->getCookieParams()['remember']);
 
-            $user = $this->database->query('SELECT `id`, `email`, `username`,`is_admin`, `active`, `remember_token`, `current_disk_quota`, `max_disk_quota` FROM `users` WHERE `remember_selector` = ? AND `remember_expire` > ? LIMIT 1',
+            $user = $this->database->query('SELECT `id`, `username`,`is_admin`, `active`, `remember_token`, `current_disk_quota`, `max_disk_quota`, `copy_raw` FROM `users` WHERE `remember_selector` = ? AND `remember_expire` > ? LIMIT 1',
                 [$selector, date('Y-m-d\TH:i:s', time())]
             )->fetch();
 
             if ($user && password_verify($token, $user->remember_token) && $user->active) {
-                $this->session->set('logged', true);
-                $this->session->set('user_id', $user->id);
-                $this->session->set('username', $user->username);
-                $this->session->set('admin', $user->is_admin);
+                $this->session->set('logged', true)
+                    ->set('user_id', $user->id)
+                    ->set('username', $user->username)
+                    ->set('admin', $user->is_admin)
+                    ->set('copy_raw', $user->copy_raw);
                 $this->setSessionQuotaInfo($user->current_disk_quota, $user->max_disk_quota);
+                $this->refreshRememberCookie($user->id);
             }
-
-            $this->refreshRememberCookie($user->id);
         }
 
         return $handler->handle($request);
