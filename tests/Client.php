f2523c24e56e6939cb3a29b3e92be36a1f23e53a
Sergio Brighenti <sergio@brighenti.me>
2020-09-27 15:47:19 +0200

    Added temporary database

--- tests/Client.php
+++ tests/Client.php
@@ -3,6 +3,7 @@
 
 namespace Tests;
 
+use App\Database\Migrator;
 use GuzzleHttp\Psr7\ServerRequest;
 use Symfony\Component\BrowserKit\AbstractBrowser;
 use Symfony\Component\BrowserKit\Response;
@@ -12,7 +13,11 @@ class Client extends AbstractBrowser
     protected function doRequest($request)
     {
         /** @var \Slim\App $app */
-        $app = require_once BASE_DIR.'bootstrap/app.php';
+        $app = require BASE_DIR.'bootstrap/app.php';
+
+        $migrator = new Migrator($app->getContainer()->get('database'), BASE_DIR.'resources/schemas');
+        $migrator->migrate();
+
         $response = $app->handle(new ServerRequest($request->getMethod(), $request->getUri(), [], $request->getContent()));
 
         return new Response($response->getBody()->getContents(), $response->getStatusCode(), $response->getHeaders());
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 
 define('BASE_DIR', realpath(__DIR__.'/../').DIRECTORY_SEPARATOR);
 define('PLATFORM_VERSION', json_decode(file_get_contents(BASE_DIR.'composer.json'))->version);
-define('CONFIG_FILE', BASE_DIR.'tests/config_test.php');
+define('CONFIG_FILE', BASE_DIR.'tests/config.test.php');
 
 ob_start();
