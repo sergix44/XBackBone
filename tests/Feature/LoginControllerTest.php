3d254a9822ba3f70beb99a32ac552a8a409aa24e
Sergio <sergio.brighenti@everli.com>
2020-10-01 18:08:35 +0200

    Test suite finally working

--- tests/Feature/LoginControllerTest.php
+++ tests/Feature/LoginControllerTest.php
@@ -22,24 +22,45 @@ class LoginControllerTest extends TestCase
         $response = $this->post(route('login.show'));
 
         $this->assertSame(302, $response->getStatusCode());
-        $this->assertSame(route('login.show'), $response->getHeader('Location'));
+        $this->assertSame(route('login.show'), $response->getHeaderLine('Location'));
     }
 
-//    /** @test */
-//    public function it_login_with_correct_data()
-//    {
-//        $this->database()->query("INSERT INTO `users` (`email`, `username`, `password`, `is_admin`, `user_code`) VALUES ('admin@example.com', 'admin', ?, 1, ?)", [password_hash('admin', PASSWORD_DEFAULT), humanRandomString(5)]);
-//
-//        $loginPage = $this->client->request('GET', route('login.show'));
-//        $form = $loginPage->selectButton('Login')->form([
-//            'username' => 'admin@example.com',
-//            'password' => 'admin',
-//        ], 'POST');
-//
-//        $this->client->submit($form);
-//        $this->client->followRedirect();
-//        dd($this->client->getResponse());
-//
-//
-//    }
+    /** @test */
+    public function it_login_with_correct_data()
+    {
+        $this->database()->query("INSERT INTO `users` (`email`, `username`, `password`, `is_admin`, `user_code`) VALUES ('admin@example.com', 'admin', ?, 1, ?)", [password_hash('admin', PASSWORD_DEFAULT), humanRandomString(5)]);
+
+        $response = $this->get(route('login.show'));
+        $form = $this->getCrawler($response)
+            ->selectButton('Login')
+            ->form([
+                'username' => 'admin@example.com',
+                'password' => 'admin',
+            ], 'POST');
+
+        $response = $this->submitForm($form);
+        $this->assertSame(302, $response->getStatusCode());
+        $this->assertSame(route('home'), $response->getHeaderLine('Location'));
+
+        $response = $this->get(route('home'));
+        $this->assertSame(200, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function it_hide_register_by_default()
+    {
+        $response = $this->get(route('login.show'));
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertStringNotContainsString('Register', $this->getCrawler($response)->text());
+    }
+
+    /** @test */
+    public function it_show_register_when_enabled()
+    {
+        $this->database()->query("INSERT INTO `settings`(`key`, `value`) VALUES ('register_enabled', 'on')");
+
+        $response = $this->get(route('login.show'));
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertStringContainsString('Register', $this->getCrawler($response)->text());
+    }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,50 +2,16 @@
 
 namespace Tests;
 
-use App\Database\DB;
 use PHPUnit\Framework\TestCase as BaseTestCase;
-use Symfony\Component\BrowserKit\Response;
 
 abstract class TestCase extends BaseTestCase
 {
-    use BootApplication;
-
-    /** @var Client */
-    protected $client;
+    use WithApplication;
 
     protected function setUp()
     {
-        $this->client = new Client($this->createApplication());
-        $this->client->followRedirects(false);
-    }
-
-    /**
-     * @param  string  $uri
-     * @param  array  $parameters
-     * @return object|Response
-     */
-    public function get(string $uri, array $parameters = [])
-    {
-        $this->client->request('GET', $uri, $parameters);
-        return $this->client->getResponse();
-    }
-
-    /**
-     * @param  string  $uri
-     * @param  array  $parameters
-     * @return object|Response
-     */
-    public function post(string $uri, array $parameters = [])
-    {
-        $this->client->request('POST', $uri, $parameters);
-        return $this->client->getResponse();
-    }
-
-    /**
-     * @return DB
-     */
-    public function database()
-    {
-        return $this->app->getContainer()->get('database');
+        parent::setUp();
+        $_SESSION = []; // ugly workaround to the the session superglobal between tests
+        $this->createApplication();
     }
 }
