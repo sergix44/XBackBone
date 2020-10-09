50c013af8b3558c173224c28128f82f07a9b6ddb
Sergio Brighenti <sergio@brighenti.me>
2020-10-09 20:01:57 +0200

    Adding register tests (#280)
    * Adding register test, faking mails
    
    * Apply fixes from StyleCI (#279)
    
    [ci skip] [skip ci]
    
    Co-authored-by: Sergio Brighenti <SergiX44@users.noreply.github.com>
    
    * Reduce method complexity
    
    Co-authored-by: Sergio Brighenti <SergiX44@users.noreply.github.com>

--- app/Web/Mail.php
+++ app/Web/Mail.php
@@ -7,6 +7,11 @@ use InvalidArgumentException;
 
 class Mail
 {
+    /**
+     * @var bool
+     */
+    private static $testing = false;
+
     protected $fromMail = 'no-reply@example.com';
     protected $fromName;
 
@@ -24,6 +29,14 @@ class Mail
     public static function make()
     {
         return new self();
+    }
+
+    /**
+     * This will skip the email send
+     */
+    public static function fake()
+    {
+        self::$testing = true;
     }
 
     /**
@@ -93,6 +106,22 @@ class Mail
     }
 
     /**
+     * Set headers before send
+     */
+    protected function setHeaders()
+    {
+        if ($this->fromName === null) {
+            $this->addRequiredHeader("From: $this->fromMail");
+        } else {
+            $this->addRequiredHeader("From: $this->fromName <$this->fromMail>");
+        }
+
+        $this->addRequiredHeader('X-Mailer: PHP/'.phpversion())
+            ->addRequiredHeader('MIME-Version: 1.0')
+            ->addRequiredHeader('Content-Type: text/html; charset=utf-8');
+    }
+
+    /**
      * @return int
      */
     public function send()
@@ -109,18 +138,14 @@ class Mail
             throw new InvalidArgumentException('Message cannot be null.');
         }
 
-        if ($this->fromName === null) {
-            $this->addRequiredHeader("From: $this->fromMail");
-        } else {
-            $this->addRequiredHeader("From: $this->fromName <$this->fromMail>");
-        }
-
-        $this->addRequiredHeader('X-Mailer: PHP/'.phpversion());
-        $this->addRequiredHeader('MIME-Version: 1.0');
-        $this->addRequiredHeader('Content-Type: text/html; charset=utf-8');
+        $this->setHeaders();
 
         $this->headers .= $this->additionalHeaders;
         $message = html_entity_decode($this->message);
+
+        if (self::$testing) {
+            return 1;
+        }
 
         return (int) mail($this->to, $this->subject, "<html><body>$message</body></html>", $this->headers);
     }
