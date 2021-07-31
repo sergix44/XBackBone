01fca619c6b9f39fa126c5165c1d80b727823db8
Sergio Brighenti <sergio@brighenti.me>
2021-07-31 13:09:45 +0200

    bump php version to 7.2

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-((PHP_MAJOR_VERSION >= 7 && PHP_MINOR_VERSION >= 1) || PHP_MAJOR_VERSION > 7) ?: die('Sorry, PHP 7.1 or above is required to run XBackBone.');
+((PHP_MAJOR_VERSION >= 7 && PHP_MINOR_VERSION >= 2) || PHP_MAJOR_VERSION > 7) ?: die('Sorry, PHP 7.2 or above is required to run XBackBone.');
 require __DIR__.'/vendor/autoload.php';
 
 define('BASE_DIR', realpath(__DIR__).DIRECTORY_SEPARATOR);
 define('PLATFORM_VERSION', json_decode(file_get_contents('composer.json'))->version);
 define('CONFIG_FILE', BASE_DIR.'config.php');
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require __DIR__.'/bootstrap/app.php';
 $app->run();
