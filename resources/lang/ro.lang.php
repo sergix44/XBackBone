57122a8f96f3fc3324d90ae6e43fe577f8031e5c
Denis Branisteanu <denisbranisteanu2001@gmail.com>
2022-07-25 10:22:21 +0000

    Translated using Weblate (Romanian)
    Currently translated at 6.2% (10 of 160 strings)
    
    Translation: XBackBone/XBackBone
    Translate-URL: https://hosted.weblate.org/projects/xbackbone/xbackbone/ro/

--- resources/lang/ro.lang.php
+++ resources/lang/ro.lang.php
@@ -1,1 +1,13 @@
 <?php
+return [
+    'login' => 'Autentificare',
+    'password' => 'Parola',
+    'lang' => 'Română',
+    'enforce_language' => 'Aplică limba',
+    'yes' => 'Da',
+    'username' => 'Utilizator',
+    'send' => 'Trimite',
+    'no_media' => 'Nu s-a gasit media.',
+    'login.username' => 'Nume de utilizator sau E-Mail',
+    'no' => 'Nu',
+];
