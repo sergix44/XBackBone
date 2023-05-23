7a4be04917a7758d5836da8b3fe92326ac670e45
Sergio Brighenti <sergio@brighenti.me>
2023-05-23 22:59:55 +0200

    fix failing test

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Middleware;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Psr7\Factory\ResponseFactory;
 
 class AuthMiddleware extends Middleware
 {
@@ -19,14 +20,14 @@ class AuthMiddleware extends Middleware
         if (!$this->session->get('logged', false)) {
             $this->session->set('redirectTo', (string) $request->getUri()->getPath());
 
-            return redirect($this->container->get(ResponseInterface::class), route('login.show'));
+            return redirect((new ResponseFactory())->createResponse(), route('login.show'));
         }
 
         if (!$this->database->query('SELECT `id`, `active` FROM `users` WHERE `id` = ? LIMIT 1', [$this->session->get('user_id')])->fetch()->active) {
             $this->session->alert(lang('account_disabled'), 'danger');
             $this->session->set('logged', false);
 
-            return redirect($this->container->get(ResponseInterface::class), route('login.show'));
+            return redirect((new ResponseFactory())->createResponse(), route('login.show'));
         }
 
         return $handler->handle($request);
