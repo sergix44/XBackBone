2b84363e197041e7f31d6b4281e7f25fd267f450
Sergio Brighenti <sergio@brighenti.me>
2024-08-28 23:27:33 +0200

    wip dashboard

---
+++ core/resources/views/components/resource.blade.php
@@ -0,0 +1,14 @@
+<div class="card image-full w-full shadow-xl group">
+    <figure>
+        <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+    </figure>
+    <div class="card-body opacity-0 group-hover:opacity-100 transition-opacity duration-150">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+        </div>
+    </div>
+</div>
--- core/resources/views/livewire/dashboard.blade.php
+++ core/resources/views/livewire/dashboard.blade.php
@@ -22,21 +22,13 @@
             <x-button icon="o-trash" class="btn-error join-item"/>
         </div>
     </div>
-    <div class="mt-5">
-        <div class="card image-full w-96 shadow-xl group">
-            <figure>
-                <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes" />
-            </figure>
-            <div class="card-body hidden group-hover:block">
-                <h2 class="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
-        </div>
+    <div class="mt-5 grid grid-cols-4 gap-4">
+        <x-resource />
+        <x-resource />
+        <x-resource />
+        <x-resource />
+        <x-resource />
+        <x-resource />
     </div>
 
 </div>
