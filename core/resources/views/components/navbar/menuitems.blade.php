0a73c5c943c1d6a7a210d8b0399aad2ed78a71b3
Sergio Brighenti <sergio@brighenti.me>
2025-04-26 01:42:06 +0200

    Refactor menu items to enhance authorization checks for settings link

--- core/resources/views/components/navbar/menuitems.blade.php
+++ core/resources/views/components/navbar/menuitems.blade.php
@@ -1,5 +1,3 @@
 <x-menu-item title="Gallery" icon="o-photo" link="{{ route('dashboard') }}"/>
 <x-menu-item title="Integrations" icon="o-cog" link="{{ route('integrations') }}"/>
-@can('administrate', '')
-    <x-menu-item title="Settings" icon="o-adjustments-vertical" link="{{ route('admin.settings') }}"/>
-@endcan
+<x-menu-item title="Settings" icon="o-adjustments-vertical" link="{{ route('admin.settings') }}" :enabled="auth()->user()?->can('administrate')"/>
