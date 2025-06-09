79c26230225bdf304d0d28e746537e9c04fc9629
Sergio Brighenti <sergio@brighenti.me>
2025-06-09 23:03:37 +0200

    Refactor profile layout and improve theme selection functionality

--- core/resources/views/livewire/user/profile.blade.php
+++ core/resources/views/livewire/user/profile.blade.php
@@ -1,14 +1,14 @@
 <div class="grid grid-cols-12 gap-6">
     <div class="md:col-span-3 col-span-12">
         <x-menu class="rounded-lg bg-base-100">
-            <x-menu-item title="Profile" icon="o-user-circle" />
-            <x-menu-item title="Tokens" icon="o-command-line" />
-            <x-menu-item title="Export Data" icon="o-arrow-right-start-on-rectangle" />
-            <x-menu-item title="Delete Account" icon="o-user-minus" class="text-red-500" />
+            <x-menu-item title="Profile" icon="o-user-circle"/>
+            <x-menu-item title="Tokens" icon="o-command-line"/>
+            <x-menu-item title="Export Data" icon="o-arrow-right-start-on-rectangle"/>
+            <x-menu-item title="Delete Account" icon="o-user-minus" class="text-red-500"/>
         </x-menu>
     </div>
-    <div class="md:col-span-9 col-span-12">
-        <div class="card bg-base-100 w-full">
+    <div class="md:col-span-9 col-span-12 flex flex-col gap-2">
+        <div class="card bg-base-100">
             <div class="card-body">
                 <x-avatar :image="$user->avatar" class="!w-22">
                     <x-slot:title class="text-3xl !font-bold pl-2">
@@ -16,12 +16,21 @@
                     </x-slot:title>
 
                     <x-slot:subtitle class="grid gap-1 mt-2 pl-2 text-xs">
-                        <x-icon name="o-paper-airplane" label="12 posts" />
-                        <x-icon name="o-chat-bubble-left" label="45 comments" />
+                        <x-icon name="o-paper-airplane" label="12 posts"/>
+                        <x-icon name="o-chat-bubble-left" label="45 comments"/>
                     </x-slot:subtitle>
                 </x-avatar>
-                <div class="divider"></div>
-                <x-select label="Theme" wire:model="theme" icon="o-paint-brush" :options="$themes"  inline />
+
+            </div>
+        </div>
+
+        <div class="card bg-base-100">
+            <div class="card-body">
+                <h2 class="card-title">Theme</h2>
+                <div class="mt-2">
+                    <x-select wire:model="theme" icon="o-paint-brush" :options="$themes" inline/>
+                    <x-button label="Save" class="btn-primary mt-4"/>
+                </div>
             </div>
         </div>
     </div>
